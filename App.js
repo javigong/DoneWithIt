e55eff41adf9c2a1c066f3dcd4a2c1347151af59
Javier Gongora <jgongora@gmail.com>
2021-12-21 15:21:21 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View, Text } from "react-native";
+
+
+import AppText from "./app/components/AppText/AppText";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>I love React Native!</AppText>
+      
+    </View>
+  );
 }
