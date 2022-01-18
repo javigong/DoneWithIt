7a2dbfb35d85846141a889ef28d52abd48168a07
Javier Gongora <jgongora@gmail.com>
2022-01-17 17:40:40 -0800

    Create AppSwitch.js

---
+++ app/components/AppSwitch.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Switch } from "react-native";
+
+function AppSwitch(props) {
+  const [isNew, setIsNew] = useState(false);
+
+  return (
+    <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+  );
+}
+
+export default AppSwitch;
