6555d76a793d7e36f1c86af6b3af917f50167160
Javier Gongora <jgongora@gmail.com>
2022-01-17 17:41:46 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
-
-import AppButton from "./app/components/AppButton";
-import AppCard from "./app/components/AppCard";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen"
 
 export default function App() {
-  return <ListingsScreen />;
+  return <ListingEditScreen /> ;
 }
